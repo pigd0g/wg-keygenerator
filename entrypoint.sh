43bf08c3e334cb9f5ff0f642c63ed12c52ee2818
pigd0g <16750317+pigd0g@users.noreply.github.com>
2023-06-07 11:18:27 +1000

    Generate Preshared Key

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
-#!/bin/ash
+#!/bin/sh
 
 PRIVATE=$(wg genkey)
 PUBLIC=$(printf $PRIVATE | wg pubkey)
+PSK=$(wg genpsk)
 
 printf "\n=================== Wireguard Key Pair ==================\n\n"
 printf "Private key: $PRIVATE\n"
 printf "Public key:  $PUBLIC\n"
+printf "Preshared key:  $PSK\n"
 printf "\n=========================================================\n"
